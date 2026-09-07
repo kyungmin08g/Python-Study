62070d030889de2ecb089d8d092cf54ab41ce17b
Kyungmin Kim <min.work0x08@gmail.com>
2026-09-07 11:16:29 +0900

    fix(type): 타입 설명 재작성(수정) 및 정리

--- beginner/basics/type.py
+++ beginner/basics/type.py
@@ -1,31 +1,39 @@
-# [ 문자열(str) ] - 문자나 문자열을 표현할 때 쓰입니다.
+# [ 문자열(str) ]
+# 문자나 문자열을 표현할 때 쓰입니다.
 name: str = "Kyungmin Kim"
-print(f"name: {name}") # name: Kyungmin Kim
-print(name is "Kyungmin") # False
+print(f"name: {name}") # (terminal print: name: Kyungmin Kim)
+print(name is "Kyungmin") # (terminal print: False)
 
 # [ 숫자(int) ] - 정수형을 표현할 때 쓰입니다.
 age: int = 20
-print(f"age: {age}") # age: 20
+print(f"age: {age}") # (terminal print: age: 20)
 
-# [ 부동소수점(float) ] - 소수점을 표현할 때 쓰입니다. (Python에는 double 타입이 없습니다.)
+# [ 부동소수점(float) ]
+# 소수점을 표현할 때 쓰입니다. (Python에는 double 타입이 없습니다.)
 weight: float = 65.3
-print(f"weight: {weight}") # weight: 65.3
+print(f"weight: {weight}") # (terminal print: weight: 65.3)
 
-# [ 배열(list) ] - 변경 가능한 배열을 필요로 할 때 쓰입니다. (mutable)
+# [ 배열(list) ]
+# 변경 가능한 배열을 필요로 할 때 쓰입니다. (mutable)
 array_data: list[int] = [1, 2, 3, 4, 5]
-print(f"array: {array_data[0]} (변경 전)") # 1
+print(f"array: {array_data[0]} (변경 전)") # (terminal print: array: 1 (변경 전))
+
 array_data[0] = 100
-print(f"array: {array_data[0]} (변경 후)") # 100
+print(f"array: {array_data[0]} (변경 후)") # (terminal print: array: 100 (변경 후))
 
-# [ 튜플(tuple) ] - 변경할 수 없는 배열을 필요로 할 때 쓰입니다. (immutable)
-tuple_data: tuple[int, str, bool] = (1, "Hello", True) # 튜플은 배열과 달리 요소를 변경할 수 없습니다. 외에는 다 list 역할과 동일합니다.
-print(f"tuple: {tuple_data}")
+# [ 튜플(tuple) ]
+# 변경할 수 없는 배열을 필요로 할 때 쓰입니다. (immutable)
+# 튜플은 배열과 달리 요소를 변경할 수 없습니다. 외에는 다 list 역할과 동일합니다.
+tuple_data: tuple[int, str, bool] = (1, "Hello", True)
+print(f"tuple: {tuple_data}") # (terminal print: tuple: (1, 'Hello', True))
 
-# [ 불(bool) ] - 참과 거짓을 표현할 때 쓰입니다. (True, False)
+# [ 불(bool) ]
+# 참과 거짓을 표현할 때 쓰입니다. (True, False)
 is_active_first: bool = True
 is_active_second: bool = False
-print(f"is_active_first: {is_active_first}, is_active_second: {is_active_second}") # is_active_first: True, is_active_second: False
+print(f"is_active_first: {is_active_first}, is_active_second: {is_active_second}") # (terminal print: is_active_first: True, is_active_second: False)
 
-# [ None ] - 값이 없음을 표현할 때 쓰입니다.
+# [ None ]
+# 값이 없음을 표현할 때 쓰입니다.
 nothing = None
-print(f"nothing: {nothing}") # nothing: None
+print(f"nothing: {nothing}") # (terminal print: nothing: None)
